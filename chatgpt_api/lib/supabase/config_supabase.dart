061b447c31b170ec9cd45a33e66504345480456b
Ruba Hilal <developerruba@outlook.com>
2023-11-29 10:07:38 +0300

    Update config_supabase.dart

--- chatgpt_api/lib/supabase/config_supabase.dart
+++ chatgpt_api/lib/supabase/config_supabase.dart
@@ -7,8 +7,7 @@ class ConfigSupabase {
 
     await Supabase.initialize(
       url: "https://zkidhsvhkjshockssdec.supabase.co",
-      anonKey:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpraWRoc3Zoa2pzaG9ja3NzZGVjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5OTc3NzIxNCwiZXhwIjoyMDE1MzUzMjE0fQ.ogA1CDmr1lRh5m5z50y-PDSVmWxtzPfeFCXMvoj7ofg",
+      anonKey: "key from supabase",
     );
   }
 }
